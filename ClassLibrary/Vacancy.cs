b99f3878345daea0007d28d0bcd19a222440c4dd
andriyShala <andriy.shala@i.ua>
2017-04-24 08:03:30 +0300

    add class Parse

--- ClassLibrary/Vacancy.cs
+++ ClassLibrary/Vacancy.cs
@@ -4,41 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassLibrary
+namespace WindowsFormsApplication1
 {
-   public class Content
+    public class Content
     {
         public string Tietle { get; set; }
         public string Teat { get; set; }
-        public DateTime Date { get; set;}
         public Content()
         {
             this.Tietle = String.Empty;
             this.Teat = String.Empty;
-            this.Date = new DateTime(1999, 01, 01);
         }
-        public Content(string Tietle,string Teat,DateTime Date)
+        public Content(string Tietle, string Teat)
         {
             this.Tietle = Tietle;
             this.Teat = Teat;
-            this.Date = Date;
         }
     }
 
-   public class Vacancy
+    public class Vacancy
     {
-       public int ID { get; set; }
+        public int ID { get; set; }
         public string Сategory { get; set; }
         public string Sity { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set;}
+        public string Company { get; set; }
+        public string Education { get; set; }
+        public string Experience { get; set; }
+        public string WorkSchedule { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Date { get; set;}
         public Content Contents { get; set; }
         public Vacancy()
         {
             this.ID = 0;
             this.Сategory = String.Empty;
-            this.Contents = null;
+            this.Contents = new Content();
             this.Sity = String.Empty;
         }
-        public Vacancy(int ID,string Category,string Sity,Content content)
+        public Vacancy(int ID, string Category, string Sity, Content content)
         {
             this.ID = ID;
             this.Сategory = Category;
