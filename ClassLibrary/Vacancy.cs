7372516a67dc2069c3056e2870894f2f60aa8b2f
andriyShala <andriy.shala@i.ua>
2017-04-26 09:59:00 +0300

    update vacancy

--- ClassLibrary/Vacancy.cs
+++ ClassLibrary/Vacancy.cs
@@ -1,55 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WindowsFormsApplication1
+﻿namespace WindowsFormsApplication1
 {
-    public class Content
-    {
-        public string Tietle { get; set; }
-        public string Teat { get; set; }
-        public Content()
-        {
-            this.Tietle = String.Empty;
-            this.Teat = String.Empty;
-        }
-        public Content(string Tietle, string Teat)
-        {
-            this.Tietle = Tietle;
-            this.Teat = Teat;
-        }
-    }
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
 
-    public class Vacancy
+    public partial class Vacancies
     {
-        public int ID { get; set; }
+
         public string Сategory { get; set; }
-        public string Sity { get; set; }
-        public string Name { get; set; }
-        public string Salary { get; set;}
-        public string Company { get; set; }
-        public string Education { get; set; }
-        public string Experience { get; set; }
-        public string WorkSchedule { get; set; }
-        public string PhoneNumber { get; set; }
-        public DateTime Date { get; set;}
-        public Content Contents { get; set; }
-        public Vacancy()
-        {
-            this.ID = 0;
-            this.Сategory = String.Empty;
-            this.Contents = new Content();
-            this.Sity = String.Empty;
-        }
-        public Vacancy(int ID, string Category, string Sity, Content content)
-        {
-            this.ID = ID;
-            this.Сategory = Category;
-            this.Contents = content;
-            this.Sity = Sity;
-        }
 
+        public string Sity { get; set; }
+
+        public string Title { get; set; }
+
+        public string Salary { get; set; }
+
+        public string Company { get; set; }
+
+        public string Education { get; set; }
+
+        public string Experience { get; set; }
+
+        public string WorkSchedule { get; set; }
+
+        public string ContactingInfo { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        public int? IdVacancysite { get; set; }
+
+        public int? Id_pars_site { get; set; }
     }
 }
