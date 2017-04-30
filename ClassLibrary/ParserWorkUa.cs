69e63d1c4758012b8a793f2e67db61edc378c86a
Ihor <moskigo2@gmail.com>
2017-04-30 10:08:36 +0300

    Installed package AngleSharp

--- ClassLibrary/ParserWorkUa.cs
+++ ClassLibrary/ParserWorkUa.cs
@@ -4,14 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ClassLibrary;
+using AngleSharp;
+
+
 namespace ClassLibrary.Work.ua
 {
+
     class ParserWorkUa : IParser
     {
+        private string site;
+        private string prefUrl;
+        private string prefPage;
+
+        private int counter;
+        IConfiguration config;
+
+        public ParserWorkUa() 
+        {
+            config = Configuration.Default.WithDefaultLoader();
+        }
+
         public List<Vacancies> StartParseAll(string keyCategory)
         {
             throw new NotImplementedException();
         }
+
 
         public List<Vacancies> StartParseforDate(string keyCategory, DateTime date)
         {
