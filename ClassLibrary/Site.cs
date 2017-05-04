66faca2af05bbc8d9d43678a676d0423077d0812
andriyShala <andriy.shala@i.ua>
2017-05-04 11:22:54 +0300

    upadate
    add methot to Class Site and ParseService

--- ClassLibrary/Site.cs
+++ ClassLibrary/Site.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace ParserService
 {
+    [Table("Sites")]
     public class Site
     {
+        [Key]
+        [Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int id { get; set; }
         public string name { get; set; }
     }
